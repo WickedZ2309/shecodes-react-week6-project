e0a7b0f0b92a643f18333fdf7151e8c086b3f1fe
WickedZ2309 <168111978+WickedZ2309@users.noreply.github.com>
2024-08-22 15:39:43 -0400

    Reverted to OpenWeather API link with SheCodes API Key

--- src/Weather.js
+++ src/Weather.js
@@ -30,8 +30,8 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = `9847fe0311559eao53bt6fd43e08ac31`;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    const apiKey = `9cb72bec958f8fb02391985ed7b219d2`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
