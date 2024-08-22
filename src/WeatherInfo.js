565af87aef62f967f822776bcecd2f40a61adeb7
WickedZ2309 <168111978+WickedZ2309@users.noreply.github.com>
2024-08-22 15:59:51 -0400

    Added Weather forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props) {
           <div className="d-flex">
             <div className="float-left">
               <div>
-                <WeatherIcon code={props.data.icon} />
+                <WeatherIcon code={props.data.icon} size={52} />
               </div>
             </div>
 
